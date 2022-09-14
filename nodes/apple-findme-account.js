f31470dd1e5e1fa71a4c757129a54f68fd71f60f
grdn1337 <dev@grdn.rocks>
2022-09-14 19:28:05 +0200

    Add first implementation of apple-findme-reloaded

---
+++ nodes/apple-findme-account.js
@@ -0,0 +1,115 @@
+const path = require('path');
+const moment = require('moment');
+const urllib = require('urllib');
+
+const pkg = require(path.join(__dirname, '..', 'package.json'));
+
+module.exports = function (RED) {
+    RED.log.info('apple-findme-reloaded version: ' + pkg.version);
+
+    const ApiEndpointUrl = 'https://fmipmobile.icloud.com/fmipservice/device/';
+
+    /**
+     * AppleFindMeAccountNode class
+     */
+    class AppleFindMeAccountNode {
+        /**
+         * Create a new account node
+         * @class
+         * @param {object} config 
+         */
+        constructor(config) {
+            RED.nodes.createNode(this, config);
+            var node = this;
+
+            this.name = config.name;
+            this.username = config.username;
+            this.password = config.password;
+
+            this.apiThrottleLimit = Number.parseInt(config.apiThrottleLimit, 10);
+            this.requestTimeout = Number.parseInt(config.requestTimeout, 10);
+
+            this.timeZone = config.timeZone;
+
+            this.lastRequestDevices = { err: null, data: null, res: null, cached: false, time: moment('19000101', 'YYYYMMDD') };
+
+            this.requestHeader = {
+                'Accept-Language': 'de-DE',
+                'User-Agent': 'FindMyiPhone/500 CFNetwork/758.4.3 Darwin/15.5.0',
+                'X-Apple-Realm-Support': '1.0',
+                'X-Apple-AuthScheme': 'UserIDGuest',
+                'X-Apple-Find-API-Ver': '3.0'
+            };
+        }
+
+        /**
+         * Refresh the auth header with actual data
+         * @private
+         */
+        _setAuthHeader() {
+            this.requestHeader['Authorization'] = `Basic ${Buffer.from(`${this.username}:${this.password}`).toString('base64')}`;
+        }
+
+        /**
+         * Request data from Apple endpoint, if not throttled. Otherwise return cached data.
+         * @async
+         * @returns {object} 
+         */
+        async requestDevices() {
+            // check if throttled
+            if (moment().diff(this.lastRequestDevices.time) < this.apiThrottleLimit * 1000) {
+                let req = this.lastRequestDevices;
+                req.cached = true;
+
+                return req;
+            }
+
+            // otherwise
+            this._setAuthHeader();
+            var node = this;
+
+            let req = new Promise(rtn => {
+                urllib.request(ApiEndpointUrl + this.username + '/initClient', {
+                    method: 'POST',
+                    dataType: 'json',
+                    headers: this.requestHeader,
+                    content: '',
+                    rejectUnauthorized: false,
+                    timeout: 5000,
+                }, function (err, data, res) {
+                    err ||= res.statusCode != 200;
+
+                    if (!err)
+                        node.lastRequestDevices = {
+                            err: err,
+                            data: data,
+                            res: res,
+                            cached: false,
+                            time: moment().tz(node.timeZone)
+                        };
+                    else
+                        node.lastRequestDevices = {
+                            err: err,
+                            data: node.lastRequestDevices.data || data,
+                            res: res,
+                            cached: true,
+                            time: moment().tz(node.timeZone)
+                        };
+                    
+                    rtn(node.lastRequestDevices);
+                });
+            });
+
+            return await req;
+        }
+    }
+
+    // create endpoint for requesting timezones from the node-red frontend
+    RED.httpAdmin.get('/apple-findme-reloaded/tz', (req, res) => {
+        let zones = moment.tz.names();
+        res.status(200).send(JSON.stringify(zones));
+    });
+
+    // register node
+    RED.nodes.registerType('apple-findme-account', AppleFindMeAccountNode);
+}
---
+++ nodes/apple-findme-request.js
@@ -0,0 +1,172 @@
+const moment = require('moment');
+
+module.exports = function (RED) {
+    /**
+     * AppleFindMeRequestNode class
+     */
+    class AppleFindMeRequestNode {
+        /**
+         * Create a new request node
+         * @class
+         * @param {object} config 
+         * @returns {void}
+         */
+        constructor(config) {
+            RED.nodes.createNode(this, config);
+            var node = this;
+
+            this.account = RED.nodes.getNode(config.account);
+            this.destKey = config.destKey;
+
+            // No processing if no account is set
+            if (!this.account) {
+                return;
+            }
+
+            this.on('input', async (msg, send, done) => {
+                this.status({ fill: "yellow", shape: "dot", text: "Requesting data..." });
+                
+                let res = msg._debug;
+                delete msg._debug;
+                if (!res)
+                    res = await this.account.requestDevices();
+
+                msg[this.destKey == '' ? 'payload' : this.destKey] = this._handleResponse(res);
+                
+                node.send(msg);
+            });
+        }
+
+        /**
+         * Handle response and set node status, return pretty payload
+         * @private
+         * @param {object} res 
+         * @returns {object}
+         */
+        _handleResponse(res) {
+            let fill = "blue";
+            let shape = "dot";
+            let text = "";
+
+            let data = this._createPayload(res.data, res.cached, res.time);
+
+            if (res.err) {
+                fill = "red";
+
+                switch (res.res.statusCode) {
+                    case 401:
+                        text = `Not authorized (${res.res.statusCode})`;
+                        break;
+                    case 403:
+                        text = `Forbidden (${res.res.statusCode})`;
+                        break;
+                    case 404:
+                        text = `Not found (${res.res.statusCode})`;
+                        break;
+                    default:
+                        text = `Unknown error (${res.res.statusCode})`;
+                }
+
+                this.status({ fill: fill, shape: shape, text: text });
+            }
+            else {
+                text = `${Object.keys(data.devices).length} devices`;
+                this.status({ fill: fill, shape: shape, text: text });
+            }
+
+            return data;
+        }
+
+        /**
+         * Create pretty payload and model the data slightly
+         * @private
+         * @param {object} data 
+         * @param {boolean} cached 
+         * @param {moment} time 
+         * @returns {object}
+         */
+        _createPayload(data, cached, time) {
+            let o = {
+                isCached: cached,
+                requestTime: time,
+                byModel: {},
+                byName: {},
+                devices: {},
+            };
+
+            if (data.content && typeof (data.content) == 'object') {
+                data.content.forEach((i) => {
+                    let d = this._generateDataModel(i, data.serverContext);
+                    o.devices[i.id] = d;
+                
+                    if (!o.byModel[d.modelName])
+                        o.byModel[d.modelName] = [];
+                    o.byModel[d.modelName].push(d);
+
+                    o.byName[d.name] = d;
+                });
+            }
+
+            return o;
+        }
+
+        /**
+         * Generate and fill model with data from Apple payload
+         * @private
+         * @param {object} item 
+         * @param {object} context 
+         * @returns {object}
+         */
+        _generateDataModel(item, context) {
+            let image_link = `${context.imageBaseUrl}/fmipmobile/deviceImages-9.0/${item.deviceClass}/${item.rawDeviceModel}-${item.deviceColor}`;
+
+            let r = {
+                id: item.id,
+                name: item.name,
+                modelName: item.modelDisplayName == "Accessory" ? item.deviceDisplayName : item.modelDisplayName,
+                className: item.deviceClass,
+                deviceName: item.deviceDisplayName,
+                rawName: item.rawDeviceModel,
+                uuid: item.baUUID,
+                discoveryId: item.deviceDiscoveryId,
+                battery: {
+                    level: item.batteryLevel,
+                    status: item.batteryStatus,
+                    isCharging: item.batteryStatus != "NotCharging",
+                    isDraining: item.batteryStatus == "NotCharging",
+                },
+                resources: {
+                    smallImage: `${image_link}/online-infobox.png`,
+                    mediumImage: `${image_link}/online-infobox__2x.png`,
+                    largeImage: `${image_link}/online-infobox__3x.png`,
+                },
+                hasLocation: false,
+                location: null,
+            };
+
+            // is not always set
+            if (item.location && typeof (item.location) == "object") {
+                r.hasLocation = true;
+
+                RED.log.info(moment);
+
+                r.location = {
+                    latitude: item.location.latitude,
+                    longitude: item.location.longitude,
+                    horizontalAccuracy: item.location.horizontalAccuracy,
+                    altitude: item.location.altitude,
+                    verticalAccuracy: item.location.verticalAccuracy,
+                    positionType: item.location.positionType,
+                    isOld: item.location.isOld,
+                    isInaccurate: item.location.isInaccurate,
+                    lastUpdated: moment(item.location.timeStamp).tz(this.account.timeZone),
+                };
+            }
+
+            return r;
+        }
+    }
+
+    // register node
+    RED.nodes.registerType('apple-findme-request', AppleFindMeRequestNode);
+};
